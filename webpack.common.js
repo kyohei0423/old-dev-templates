775687e920558a1b96915e0d61d927b6b4a4db3c
Kyohei Seo <kyo199333@gmail.com>
2019-05-20 12:15:40 +0900

    webpack-dev-serverでwebサーバを立てる

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "App name",
+      template: "index.html",
+      inject: false
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  }
+};
