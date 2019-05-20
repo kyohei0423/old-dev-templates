28250a7c2891a02267c88f1dce8561cc575576ef
Kyohei Seo <kyo199333@gmail.com>
2019-05-20 12:21:08 +0900

    Add webpack configuration for production

---
+++ webpack.prod.js
@@ -0,0 +1,16 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "production",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /\.js(\?.*)?$/i,
+        parallel: true
+      })
+    ]
+  },
+  plugins: []
+});
